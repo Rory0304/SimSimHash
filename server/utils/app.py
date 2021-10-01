51f2e52619db31ca5f0479aab10bf30b65a055ef
gon2gon2 <ark5354@gmail.com>
2021-10-01 15:04:32 +0900

    ORM

---
+++ server/utils/app.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, render_template, redirect, url_for
+
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+import config
+
+db = SQLAlchemy()
+migrate = Migrate()
+
+def create_app():
+    
+    from models.movie import 
+    
+    app = Flask(__name__)
+    app.config.from_object(config)
+    app.secret_key = "hahaha"
+
+    from .views import user_views, book_views, rental_views
+    app.register_blueprint(user_views.bp)
+    app.register_blueprint(book_views.bp)
+    app.register_blueprint(rental_views.bp)
+    
+    # ORM
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    @app.route('/', methods=['GET'])
+    def home():
+        return "hello"
+    
+    
+    return app

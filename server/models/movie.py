93d0e493c4d556f643180d210f97905ceaf29925
gon2gon2 <ark5354@gmail.com>
2021-10-05 13:18:33 +0900

    change MOVIE table

--- server/models/movie.py
+++ server/models/movie.py
@@ -11,6 +11,12 @@ class Movie(db.Model):
     running_time = db.Column(db.Integer)
     poster = db.Column(db.String(200))
     genre = db.Column(db.String(50))
+    rating = db.Column(db.Float)
+    naver = db.Column(db.Float)
+    daum = db.Column(db.Float)
+    watcha = db.Column(db.Float)
+    cine21 = db.Column(db.Float)
+    
     
     def __init__(self,
                 name, 
@@ -20,7 +26,12 @@ class Movie(db.Model):
                 summary=None, 
                 running_time=None, 
                 poster=None,
-                genre=None):        
+                genre=None,
+                rating = None,
+                naver = None,
+                daum = None,
+                watcha = None,
+                cine21 = None):        
         self.name = name
         self.screening_date = screening_date
         self.actor = actor
@@ -29,3 +40,8 @@ class Movie(db.Model):
         self.running_time = running_time
         self.poster = poster
         self.genre = genre
+        self.rating = rating
+        self.naver = naver
+        self.daum = daum
+        self.watcha = watcha
+        self.cine21 = cine21

e18a8dfce0aebbee71aa845227a4c5ed8d9ab0ea
[2기-2반] 사은수 <mitty0304@naver.com>
2021-10-06 15:54:11 +0900

    Fix: 영화 목록에 해시태그 추가

--- client/src/pages/Main/MainPage.jsx
+++ client/src/pages/Main/MainPage.jsx
@@ -17,37 +17,37 @@ const sample = [
         img: "https://movie-phinf.pstatic.net/20211001_121/16330536363187liBB_JPEG/movie_image.jpg",
         title: "007 노 타임 투 다이",
         star: 7.29,
-        tag: "웅장한"
+        tag: "#웅장한"
     },
     {
         img: "https://movie-phinf.pstatic.net/20210831_152/1630371935224qy9s6_JPEG/movie_image.jpg",
         title: "보이스",
         star: 6.42,
-        tag: "감동적인"
+        tag: "#감동적인"
     },
     {
         img: "https://movie-phinf.pstatic.net/20210917_60/16318534287592XLg8_JPEG/movie_image.jpg",
         title: "기적",
         star: 8.49,
-        tag: "재치있는"
+        tag: "#재치있는"
     },
     {
         img: "https://movie-phinf.pstatic.net/20210825_8/1629879510900xEwsL_JPEG/movie_image.jpg",
         title: "샹치와 텐 링즈의 전설",
         star: 7.29,
-        tag: "의미있는"
+        tag: "#의미있는"
     },
     {
         img: "https://movie-phinf.pstatic.net/20210831_7/1630399407921MTlDg_JPEG/movie_image.jpg",
         title: "스쿨 아웃 포에버",
         star: 7.29,
-        tag: "독특한"
+        tag: "#독특한"
     },
     {
         img: "https://movie-phinf.pstatic.net/20210820_122/1629448997924YXUCv_JPEG/movie_image.jpg",
         title: "모가디슈",
         star: 7.29,
-        tag: "화려한"
+        tag: "#화려한"
     },
     {
         img: "https://movie-phinf.pstatic.net/20210930_111/1632990544838Hy8aq_JPEG/movie_image.jpg",
@@ -59,7 +59,7 @@ const sample = [
         img: "https://movie-phinf.pstatic.net/20211001_121/16330536363187liBB_JPEG/movie_image.jpg",
         title: "포켓몬스터: 정글의 아이, 코코",
         star: 7.29,
-        tag: "재미있는"
+        tag: "#재미있는"
     }
 ];
 
@@ -73,7 +73,6 @@ function MainPage() {
             return dispatch(getMovieListByTag());
         }, 1000);
         return () => {
-            console.log("clear");
             clearTimeout(debounce);
         };
     }, [selectedTagList]);

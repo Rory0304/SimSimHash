d7fbfef3e84a95c0f52a9205c6bb985a30b35d17
JihoPark <diorpark@gmail.com>
2021-10-06 15:06:15 +0900

    Test: carousel과 modal에 filter 추가

--- client/src/pages/Main/MainPage.jsx
+++ client/src/pages/Main/MainPage.jsx
@@ -12,37 +12,37 @@ const sample = [
         img: "https://movie-phinf.pstatic.net/20211001_121/16330536363187liBB_JPEG/movie_image.jpg",
         title: "007 노 타임 투 다이",
         star: 7.29,
-        tag: "웅장한"
+        tag: "#웅장한"
     },
     {
         img: "https://movie-phinf.pstatic.net/20210831_152/1630371935224qy9s6_JPEG/movie_image.jpg",
         title: "보이스",
         star: 6.42,
-        tag: "감동적인"
+        tag: "#감동적인"
     },
     {
         img: "https://movie-phinf.pstatic.net/20210917_60/16318534287592XLg8_JPEG/movie_image.jpg",
         title: "기적",
         star: 8.49,
-        tag: "재치있는"
+        tag: "#재치있는"
     },
     {
         img: "https://movie-phinf.pstatic.net/20210825_8/1629879510900xEwsL_JPEG/movie_image.jpg",
         title: "샹치와 텐 링즈의 전설",
         star: 7.29,
-        tag: "의미있는"
+        tag: "#의미있는"
     },
     {
         img: "https://movie-phinf.pstatic.net/20210831_7/1630399407921MTlDg_JPEG/movie_image.jpg",
         title: "스쿨 아웃 포에버",
         star: 7.29,
-        tag: "독특한"
+        tag: "#독특한"
     },
     {
         img: "https://movie-phinf.pstatic.net/20210820_122/1629448997924YXUCv_JPEG/movie_image.jpg",
         title: "모가디슈",
         star: 7.29,
-        tag: "화려한"
+        tag: "#화려한"
     },
     {
         img: "https://movie-phinf.pstatic.net/20210930_111/1632990544838Hy8aq_JPEG/movie_image.jpg",
@@ -54,7 +54,7 @@ const sample = [
         img: "https://movie-phinf.pstatic.net/20211001_121/16330536363187liBB_JPEG/movie_image.jpg",
         title: "포켓몬스터: 정글의 아이, 코코",
         star: 7.29,
-        tag: "재미있는"
+        tag: "#재미있는"
     }
 ]
 
@@ -86,7 +86,7 @@ function MainPage() {
             filter.push(item.name)
         });
         setFilteredMovieList(sample.filter(movie => 
-            movie.tag === filter))
+            filter.includes(movie.tag)))
     }, [selectedTagList]);
 
     const onSelectTag = (tag) => {
@@ -138,7 +138,7 @@ function MainPage() {
         return (
             <>
                 <Slider {...settings}>
-                    {sample.map((item) => {
+                    {filteredMovieList.map((item) => {
                         return (
                             <>
                                 <img src={item.img} width="400"></img>
@@ -179,7 +179,7 @@ function MainPage() {
                     더 보기
                 </Button>
                 <Modal title="Basic Modal" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
-                    {sample.map((item) => {
+                    {filteredMovieList.map((item) => {
                         return (
                             <>
                                 <img src={item.img} width="400"></img>

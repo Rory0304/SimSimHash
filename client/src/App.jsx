f95a603f6bd873bfa4ed257a1a88dfdc8b3c7899
[2기-2반] 사은수 <mitty0304@naver.com>
2021-10-07 10:34:17 +0900

    Chore: js를 jsx로 변경

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,10 @@
+/** @jsxImportSource @emotion/react */
+
 import { Provider } from "react-redux";
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import Router from "./Router";
 import mainTagDataSlice from "./modules/MainPage/tagDataSlice";
+import { css, jsx } from "@emotion/react";
 
 import "./App.css";
 
@@ -16,11 +19,15 @@ function App() {
 
     return (
         <Provider store={store}>
-            <div className="App">
+            <div css={wrapperStyle}>
                 <Router />
             </div>
         </Provider>
     );
 }
+
+const wrapperStyle = css`
+    background-color: #24262b;
+`;
 
 export default App;

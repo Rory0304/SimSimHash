21e1f6153986c94f6f06eca57ee1488c438ddb42
JihoPark <diorpark@gmail.com>
2021-10-07 10:35:21 +0900

    Style: carousel 디자인 수정

--- client/src/pages/Main/MainPage.jsx
+++ client/src/pages/Main/MainPage.jsx
@@ -1,3 +1,6 @@
+/** @jsxImportSource @emotion/react */
+import { css, jsx } from '@emotion/react'
+
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
@@ -8,19 +11,87 @@ import React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import { Button, Modal } from "antd";
+import { Button, Modal, Rate } from "antd";
+
+const divStyle = css`
+    background-color: #333333;
+    width: 40%;
+    height: 300px;
+    text-align: center;
+    margin: 0 auto;
+    padding-top: 20px;
+    img {
+        width: 100px;
+        margin: 0 auto;
+    }
+    .slick-slider {
+        margin:0 15px;
+    }
+    .slick-slide {
+        width: 150px;
+        padding: 10px;
+        border-radius: 15px;
+        background-color: #444444;
+        text-align: center;
+        margin-right: 15px;
+        margin-left: 15px;
+    }
+    .slick-prev:before {
+        opacity: 1;
+        color: black;
+        left: 0;
+    }
+    .slick-next:before {
+        opacity: 1;
+        color: black;
+    }
+`
+
+const modalStyle = css`
+    display: grid;
+    img {
+        width: 100px;
+        margin: 0 auto;
+    }
+    .ant-modal {
+        width: 500px;
+        background-color: #111111;
+        color: red;
+        /*overflow-y: auto;*/
+        /*overflow-x: auto;*/
+    }
+    .ant-modal-content {
+        width: 500px;
+        background-color: #111111;
+        color: red;
+        /*overflow-y: auto;*/
+        /*overflow-x: auto;*/
+    }
+    .ant-modal-body {
+        background-color: #111111;
+    }
+`
+
+const rateStyle = css`
+    font-size: 13px;
+`
+
+const fontStyle = css`
+    font-size: 13px;
+    color: #ffffff;
+`
 
 const sample = [
     {
         img: "https://movie-phinf.pstatic.net/20211001_121/16330536363187liBB_JPEG/movie_image.jpg",
         title: "007 노 타임 투 다이",
-        star: 7.29,
+        star: 9.29,
         tag: "#웅장한"
     },
     {
         img: "https://movie-phinf.pstatic.net/20210831_152/1630371935224qy9s6_JPEG/movie_image.jpg",
         title: "보이스",
-        star: 6.42,
+        star: 3.42,
         tag: "#감동적인"
     },
     {
@@ -32,19 +103,19 @@ const sample = [
     {
         img: "https://movie-phinf.pstatic.net/20210825_8/1629879510900xEwsL_JPEG/movie_image.jpg",
         title: "샹치와 텐 링즈의 전설",
-        star: 7.29,
+        star: 9.49,
         tag: "#의미있는"
     },
     {
         img: "https://movie-phinf.pstatic.net/20210831_7/1630399407921MTlDg_JPEG/movie_image.jpg",
         title: "스쿨 아웃 포에버",
-        star: 7.29,
+        star: 6.29,
         tag: "#독특한"
     },
     {
         img: "https://movie-phinf.pstatic.net/20210820_122/1629448997924YXUCv_JPEG/movie_image.jpg",
         title: "모가디슈",
-        star: 7.29,
+        star: 5.29,
         tag: "#화려한"
     },
     {
@@ -54,9 +125,9 @@ const sample = [
         tag: "#멋있는"
     },
     {
-        img: "https://movie-phinf.pstatic.net/20211001_121/16330536363187liBB_JPEG/movie_image.jpg",
-        title: "포켓몬스터: 정글의 아이, 코코",
-        star: 7.29,
+        img: "https://movie-phinf.pstatic.net/20210824_61/1629789980296uFIEw_JPEG/movie_image.jpg",
+        title: "극장판 포켓몬스터: 정글의 아이, 코코 ",
+        star: 5.79,
         tag: "#재미있는"
     }
 ];
@@ -92,6 +163,7 @@ function MainPage() {
             slidesToShow: 4,
             slidesToScroll: 4,
             initialSlide: 0,
+            variableWidth: true,
 
             responsive: [
                 {
@@ -130,11 +202,13 @@ function MainPage() {
                         return (
                             <>
                                 <img src={item.img} width="400"></img>
-                                {item.title}
-                                <br />
-                                {item.star}
-                                <br />
-                                {item.tag}
+                                <p css={fontStyle}>
+                                    {item.title}
+                                </p>
+                                <Rate disabled allowHalf defaultValue={Math.round(item.star/2)} css={rateStyle} />
+                                <p css={fontStyle}>
+                                    {item.tag}
+                                </p>
                             </>
                         );
                     })}
@@ -164,32 +238,40 @@ function MainPage() {
                 <p>태그를 선택하여 새로운 영화 취향을 발견해보세요!</p>
             </div>
             <div>
-                <div>
+                <div css={divStyle}>
                     <Slide />
                 </div>
-                <br />
-                <Button type="primary" onClick={showModal}>
-                    더 보기
-                </Button>
-                <Modal
-                    title="Basic Modal"
-                    visible={isModalVisible}
-                    onOk={handleOk}
-                    onCancel={handleCancel}
-                >
-                    {filteredMovieList.map((item) => {
-                        return (
-                            <>
-                                <img src={item.img} width="400"></img>
-                                {item.title}
-                                <br />
-                                {item.star}
-                                <br />
-                                {item.tag}
-                            </>
-                        );
-                    })}
-                </Modal>
+                <div>
+                    <Button type="primary" onClick={showModal}>
+                        더 보기
+                    </Button>
+                </div>
+                <div css={modalStyle}>
+                    <Modal
+                        title="Basic Modal"
+                        visible={isModalVisible}
+                        onOk={handleOk}
+                        onCancel={handleCancel}
+                        bodyStyle={{
+                            backgroundColor: '#333333'
+                        }}
+                    >
+                        {filteredMovieList.map((item) => {
+                            return (
+                                <>
+                                    <img src={item.img} width="100"></img>
+                                    <p css={fontStyle}>
+                                        {item.title}
+                                    </p>
+                                    <Rate disabled allowHalf defaultValue={Math.round(item.star/2)} css={rateStyle} />
+                                    <p css={fontStyle}>
+                                        {item.tag}
+                                    </p>
+                                </>
+                            );
+                        })}
+                    </Modal>
+                </div>
                 <SelectedTagList />
                 <TagList />
             </div>

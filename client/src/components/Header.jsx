abd542fb2cc3bacfe3306c92042739586bad591b
[2기-2반] 사은수 <mitty0304@naver.com>
2021-10-07 10:39:15 +0900

    Style: 헤더 디자인 추가

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -25,18 +25,32 @@ function Header() {
 const headerStyle = css`
     display: flex;
     justify-content: space-between;
-    height: 74px;
-    line-height: 74px;
-    padding: 0 28px;
+    height: 80px;
+    line-height: 80px;
+    padding: 0 65px;
+    border-bottom: 1px solid;
+    border-color: #95959500;
+    box-shadow: -1px -5px 16px 8px rgb(0 0 0 / 24%);
+
+    a {
+        color: rgba(255, 255, 255, 0.8);
+        display: block;
+    }
+
+    h1 {
+        font-size: 1.6rem;
+        color: rgba(255, 255, 255, 0.8);
+        font-weight: bold;
+    }
 `;
 
 const menuStyle = css`
     display: flex;
     list-style: none;
     justify-content: space-around;
-
+    font-size: 1.25rem;
     li {
-        width: 100px;
+        width: 150px;
         text-align: center;
     }
 `;

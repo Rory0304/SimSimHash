a94c5a6a6840dad41b188ee16e6666e86cd56dc1
[2기-2반] 사은수 <mitty0304@naver.com>
2021-10-07 10:43:44 +0900

    Feat: 태그 개수 15개로 조절

--- client/src/modules/MainPage/tagDataSlice.js
+++ client/src/modules/MainPage/tagDataSlice.js
@@ -6,6 +6,7 @@ const initialState = {
     movieList: []
 };
 
+//15개씩 받아온다.
 export const getTagList = createAsyncThunk("GET_TAG_DATA", (args, ThunkAPI) => {
     const requested = [
         { key: 1, name: "#재미있는" },
@@ -16,7 +17,13 @@ export const getTagList = createAsyncThunk("GET_TAG_DATA", (args, ThunkAPI) => {
         { key: 6, name: "#재치있는" },
         { key: 7, name: "#웅장한" },
         { key: 8, name: "#멋있는" },
-        { key: 9, name: "#거대한" }
+        { key: 9, name: "#거대한" },
+        { key: 10, name: "#위대한" },
+        { key: 11, name: "#절망적인" },
+        { key: 12, name: "#비극적인" },
+        { key: 13, name: "#광기의" },
+        { key: 14, name: "#블랙코미디" },
+        { key: 15, name: "#가족" }
     ];
     return requested;
 });
@@ -24,15 +31,21 @@ export const getTagList = createAsyncThunk("GET_TAG_DATA", (args, ThunkAPI) => {
 //임시 테스트 용
 export const getNewTagList = createAsyncThunk("GET_NEWTAG_DATA", (args, ThunkAPI) => {
     const requested = [
-        { key: 10, name: "#재미있는" },
+        { key: 1, name: "#재미있는" },
         { key: 2, name: "#독특한" },
-        { key: 12, name: "#유쾌한" },
+        { key: 3, name: "#유쾌한" },
         { key: 4, name: "#감동적인" },
-        { key: 14, name: "#의미있는" },
-        { key: 15, name: "#재치있는" },
         { key: 7, name: "#웅장한" },
-        { key: 17, name: "#멋있는" },
-        { key: 18, name: "#거대한" }
+        { key: 5, name: "#의미있는" },
+        { key: 6, name: "#재치있는" },
+        { key: 8, name: "#멋있는" },
+        { key: 9, name: "#거대한" },
+        { key: 20, name: "#아름다운" },
+        { key: 21, name: "#공포스러운" },
+        { key: 22, name: "#희극의" },
+        { key: 23, name: "무서운" },
+        { key: 24, name: "인연" },
+        { key: 25, name: "공포" }
     ];
     return requested;
 });
@@ -56,7 +69,7 @@ export const mainTagDataSlice = createSlice({
             state.selectedTagList = filteredTagList;
         },
         setTagList(state, action) {
-            state.tagList = action.payload.tagList;
+            Object.assign(state.tagList, action.payload.tagList);
         }
     }
 });

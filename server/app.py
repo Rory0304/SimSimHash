e26e53c25e3e40a5e1b1b8dba92751e985b46d31
gon2gon2 <ark5354@gmail.com>
2021-10-09 11:46:02 +0900

    add config

--- server/app.py
+++ server/app.py
@@ -1,8 +1,13 @@
-from flask import Flask, request, render_template, redirect, url_for
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import config
+import pymysql
+pymysql.install_as_MySQLdb()
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_object(config)
 
     # Blueprints
     from blueprints import aboutus, detail, movie, search, tag
@@ -13,11 +18,11 @@ def create_app():
     app.register_blueprint(tag.bp)
     
     # ORM
-    db = SQLAlchemy()
     db.init_app(app)
     
+
     return app
 
-#컨테이너 작성시 0.0.0.0 아이피로 수정필요
 if __name__ == "__main__":
-    create_app().run(debug=True)
+    app = create_app()
+    app.run(host='0.0.0.0', debug=True)

7e090ca69b4b0cab063d2677a0f5faa543869e1d
rkdnddl <rkdnddl@gmail.com>
2021-10-12 14:26:14 +0900

    branch clean up

--- server/config.py
+++
@@ -1,6 +0,0 @@
-SQLALCHEMY_DATABASE_URI = '''mysql://root:7teamghkdlxld@kdt-vm-0202007.koreacentral.cloudapp.azure.com:5000/simsimhash?charset=utf8'''
-MONGO_URI = 'mongodb://kdt-vm-0202007.koreacentral.cloudapp.azure.com:21/'
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-JSON_AS_ASCII = False
-SECRET_KEY = "hahaha"
-

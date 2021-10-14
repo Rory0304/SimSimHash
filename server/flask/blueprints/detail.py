4739ebe323cb004077ad4a477c4599ba0f7f4bba
rkdnddl <rkdnddl@gmail.com>
2021-10-15 02:26:20 +0900

    detail update

--- server/flask/blueprints/detail.py
+++ server/flask/blueprints/detail.py
@@ -1,7 +1,7 @@
 from re import search
 from flask import Blueprint, jsonify, request
 from models.movie import Movie
-from app import hashtag_col
+from app import hashtag_col, review_col
 
 bp = Blueprint('detail',__name__)
 
@@ -11,18 +11,18 @@ def get_detail():
 
     movie_detail = get_movie_detail(movie_id)
     total_detail = get_total_detail(movie_id)
+    platform_summary = get_platform_summary(movie_id)
 
-    return jsonify(movie_detail, total_detail)
+    return jsonify({"detail": movie_detail, "total": total_detail, "platform_summary": platform_summary})
 
 @bp.route("/detail/get_platform", methods=['POST'])
 def get_platform():
     movie_id = request.json['movie_id']
     platform = request.json['platform']
 
-    movie_detail = get_movie_detail(movie_id)
-    total_detail = get_total_detail(movie_id)
-
-    return jsonify(movie_detail, total_detail)
+    get_platform_tag(movie_id, platform)
+    
+    return jsonify()
 
 
 def get_movie_detail(movie_id):
@@ -47,23 +47,34 @@ def get_total_detail(movie_id):
 
     searched_movie = {
         "score": filtered_movie.score,
-        "naver": filtered_movie.naver,
-        "daum": filtered_movie.daum,
-        "watcha": filtered_movie.watcah,
-        "cine21": filtered_movie.cine21
+        "tags": list(get_platform_tag(movie_id, "total"))
     }
 
     return searched_movie
 
-@bp.route('/detail/wc')
-def wc():
-    movie_id = request.json.get('movie_id')
+def get_platform_summary(movie_id):
+    filtered_movie = Movie.query.filter(Movie.id == movie_id).first()
 
-    cur = hashtag_col.find({"movie_id": movie_id})
-    return jsonify({
-        'total': cur['total'],
-        'naver':cur['naver'],
-        'daum':cur['daum'],
-        'watcha':cur['watcha'],
-        'cine21':cur['cine21']
-    })
+    searched_movie = {
+        "naver": filtered_movie.naver,
+        "daum": filtered_movie.daum,
+        "watcha": filtered_movie.watcha,
+        "cine21": filtered_movie.cine21,
+        # "naver_count": ,
+        # "daum_count":,
+        # "watcha_count":,
+        # "cine21_count":,
+        "naver_tag": list(get_platform_tag(movie_id, "naver"))[0: 6],
+        "daum_tag": list(get_platform_tag(movie_id, "daum"))[0: 6],
+        "watcha_tag": list(get_platform_tag(movie_id, "watcha"))[0: 6],
+        "cine21_tag": list(get_platform_tag(movie_id, "cine21"))[0: 6]
+    }
+
+    return searched_movie
+
+def get_platform_tag(movie_id, platform):
+    #movie_id int처리
+    tags = hashtag_col.find_one({"movie_id": int(movie_id)})
+    platform_tags = tags[platform]
+
+    return platform_tags

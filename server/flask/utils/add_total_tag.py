0ea6990a8de281a09d75af94284d18850ef15a98
rkdnddl <rkdnddl@gmail.com>
2021-10-14 19:12:33 +0900

    add_totaltag_update

--- server/flask/utils/add_total_tag.py
+++ server/flask/utils/add_total_tag.py
@@ -4,6 +4,7 @@ sys.path.append(path.dirname( path.dirname( path.abspath(__file__) ) ))
 
 import pandas as pd
 from app import review_col, hashtag_col, client
+import time
 
 naver = pd.read_csv("./data/review/Okt_keyword50.csv")
 daum = pd.read_csv("./data/review/Okt_keyword_50daum.csv")
@@ -11,14 +12,30 @@ daum = pd.read_csv("./data/review/Okt_keyword_50daum.csv")
 naver = naver['noun']
 daum = daum['noun']
 
-insert_data = {}
+
+hashtag_col.insert_one({"total_tag": "true"})
+
+sleep_time = 0
 index = 0
 for noun in naver:
-    insert_data[str(index)] = noun
+    hashtag_col.update({"total_tag": "true"}, {"$push": { "content": {"index": str(index), "tag": noun}}})
     index = index + 1
+    print(sleep_time)
+
+    sleep_time =sleep_time + 1
+    if sleep_time == 200:
+        time.sleep(5)
+        sleep_time = 0
+
 
 for noun in daum:
-    insert_data[str(index)] = noun
+    hashtag_col.update({"total_tag": "true"}, {"$push": { "content": {"index": str(index), "tag": noun}}})
+    # time.sleep(0.9)
     index = index + 1
 
-hashtag_col.insert({"total_tag": "true", "content": insert_data})
+    sleep_time =sleep_time + 1
+    if sleep_time == 30:
+        time.sleep(5)
+        sleep_time = 0
+
+

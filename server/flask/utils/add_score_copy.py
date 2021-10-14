8a93081293d2543eb09f72295790ed73403af9bd
gon2gon2 <ark5354@gmail.com>
2021-10-14 10:14:00 +0900

    asdas

---
+++ server/flask/utils/add_score_copy.py
@@ -0,0 +1,63 @@
+from re import M
+import sys
+from os import path
+sys.path.append(path.dirname( path.dirname( path.abspath(__file__) ) ))
+
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from pymongo import MongoClient
+import pymysql
+pymysql.install_as_MySQLdb()
+
+from sqlalchemy import desc
+from models.movie import Movie
+
+from app import ranking_col, hashtag_col, review_col
+from config import SQLALCHEMY_DATABASE_URI
+from pprint import pprint
+
+engine = create_engine(SQLALCHEMY_DATABASE_URI)
+Session = sessionmaker(bind=engine)
+session = Session()
+
+
+projection = {"_id": False}
+cur = review_col.find({'movie_id': 770, "source_site":'watchapedia'}, projection)
+
+score_dict = {
+        'total' : [],
+        'naver' : [],
+        'daum' : [],
+        'watchapedia' : [],
+        'cine21' : [],
+    }
+
+for review in cur:
+    source_site = review["source_site"]
+    score = review["score"]
+    
+    try: # 간혹 score에 숫자가 아닌 값이 들어가서 예외 처리
+        score_dict['total'].append(float(score))
+        score_dict[source_site].append(float(score))
+        
+    except:
+        pass
+print(score_dict)
+# zero division error 방지
+n_total = max(len(score_dict["total"]), 1)
+n_naver = max(len(score_dict["naver"]), 1)
+n_daum = max(len(score_dict["daum"]), 1)
+n_watchpedia = max(len(score_dict["watchapedia"]), 1)
+n_cine21 = max(len(score_dict["cine21"]), 1)
+    
+total_score = sum(score_dict["total"]) / n_total
+naver_score = sum(score_dict["naver"]) / n_naver
+daum_score = sum(score_dict["daum"]) / n_daum
+watcha_score = sum(score_dict["watchapedia"]) / n_watchpedia
+cine21_score = sum(score_dict["cine21"]) / n_cine21
+
+
+
+
+print(total_score)
+print(watcha_score/2)
